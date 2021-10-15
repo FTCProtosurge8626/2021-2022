bd27fd67b53c3abf3ba74d8590e35b188c3a58f8
MarcusTurley <86125719+MarcusTurley@users.noreply.github.com>
2021-10-15 15:27:41 -0500

    Update Motor_Hardware.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Motor_Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Motor_Hardware.java
@@ -5,16 +5,19 @@ import static org.firstinspires.ftc.teamcode.Hardware.Robot_Hardware.Motors;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-public class Motor_Hardware {
-	public static void InitMotors(){
-		Motors[0].setPower(0);
-		Motors[1].setPower(0);
-		Motors[2].setPower(0);
-		Motors[3].setPower(0);
+public class Motor_Hardware extends CustomLinearOpMode {
+	public static void InitMotors() {
+		//Sets the Motor names on the phone
+		Motors[0] = hardwareMap.get(DcMotor.class, "RightFront");
+		Motors[1] = hardwareMap.get(DcMotor.class, "RightRear");
+		Motors[2] = hardwareMap.get(DcMotor.class, "LeftFront");
+		Motors[3] = hardwareMap.get(DcMotor.class, "LeftRear");
+		//Sets the Motor direction to zero
 		Motors[0].setDirection(DcMotorSimple.Direction.FORWARD);
 		Motors[1].setDirection(DcMotorSimple.Direction.FORWARD);
 		Motors[2].setDirection(DcMotorSimple.Direction.FORWARD);
 		Motors[3].setDirection(DcMotorSimple.Direction.FORWARD);
+		//Sets the Motor to break when the setPower is zero instead of keeping its momentum
 		Motors[0].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 		Motors[1].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 		Motors[2].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -22,7 +25,7 @@ public class Motor_Hardware {
 	}
 
 	//Reset and turn on Encoders
-	public static void runEncoder(){
+	public static void runEncoder() {
 		Motors[0].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 		Motors[1].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 		Motors[2].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
