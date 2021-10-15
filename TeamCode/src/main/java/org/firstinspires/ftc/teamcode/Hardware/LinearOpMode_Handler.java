a62d7c237fdb4dc21f30eec97087498e59a7d4f1
MarcusTurley <86125719+MarcusTurley@users.noreply.github.com>
2021-10-15 15:34:06 -0500

    Create LinearOpMode_Handler.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/LinearOpMode_Handler.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.LinearOpMode_Handler;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Linear_OpMode_Handler(){
+	@Override
+	public void runOpMode() throws InterruptedException { }
+}
