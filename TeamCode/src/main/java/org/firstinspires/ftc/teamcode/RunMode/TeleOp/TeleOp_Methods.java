e15a7a4046fe148a23834b81c63f9cad8b9558de
MarcusTurley <86125719+MarcusTurley@users.noreply.github.com>
2021-10-20 11:04:33 -0500

    Update TeleOp_Methods.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunMode/TeleOp/TeleOp_Methods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunMode/TeleOp/TeleOp_Methods.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.RunMode.TeleOp;
 import static org.firstinspires.ftc.teamcode.Hardware.ClassHardware.IMU_Hardware.*;
 import static org.firstinspires.ftc.teamcode.Hardware.ClassHardware.Motor_Hardware.*;
 import static org.firstinspires.ftc.teamcode.Hardware.Robot_Hardware.DriveMode.*;
+import static org.firstinspires.ftc.teamcode.Framework.Convert.*;
 
 import org.firstinspires.ftc.teamcode.Hardware.Robot_Hardware;
 
@@ -11,6 +12,14 @@ public class TeleOp_Methods extends Robot_Hardware {
 	private static double[] powers = new double[4];
 
 	public static void move(double forward, double horizontal, double rotational) {
+		double totalPower = Math.abs(forward) + Math.abs(horizontal) + Math.abs(rotational)
+			
+		if(totalPower > 1){
+			forward =/ totalPower;
+			horizontal =/ totalPower;
+			rotational =/ totalPower;
+		}
+		
 		switch(driveMode) {
 			case Tank:
 				powers[0] = forward + rotational;
@@ -29,25 +38,20 @@ public class TeleOp_Methods extends Robot_Hardware {
 			default:
 				break;
 		}
-
 		SetPowers(Motors, powers);
-
 	}
 
-	public static void moveHeading(double forward, double horizontal, double rotational) {
-
+	public static void moveHeading(double forward, double horizontal, double rotational, boolean pauseHeading, boolean correctHeading) {
 		if(rotational != 0) {
 			SetTargetHeading(getTargetHeading());
 		}
+		
+		double error = normalize(getHeadingError(getTargetHeading()),-180,180);
 
-		double error = getHeadingError(getTargetHeading()) * 0.05;
-
-		if(forward == 0 || horizontal == 0){
-			move(forward, horizontal, error + rotational);
+		move(forward, horizontal, error + rotational);
+		
+		if(pauseHeading){
+			move(forward, horizontal, rotational);
 		}
-
 	}
-
-
-
 }
