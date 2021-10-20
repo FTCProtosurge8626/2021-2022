6141f6e636ea41e33ffabc04555629759e49963c
MarcusTurley <86125719+MarcusTurley@users.noreply.github.com>
2021-10-20 10:01:21 -0500

    Update Convert.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Convert.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Convert.java
@@ -2,12 +2,16 @@ package org.firstinspires.ftc.teamcode.Framework;
 
 public class Convert {
 	//Converts any number to boolean
-	private static boolean toBoolean(int value) {
+	public static boolean toBoolean(int value) {
 		return value != 0;
 	}
 
 	//Converts boolean to int
-	private static int toInt(boolean input) {
+	public static int toInt(boolean input) {
 		return input ? 1 : 0;
+	}
+	
+	public double normalize(double value, double min, double max) {
+    		return 1 - ((value - min) / (max - min));
 	}
 }
