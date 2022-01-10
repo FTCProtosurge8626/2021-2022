438707211ea5f40e9dc93254e6e81880e40e0fe3
UME Robotics <robotics@umeprep.org>
2022-01-10 16:19:52 -0600

    1/10/22 - Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mach/IMUTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mach/IMUTeleOp.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -123,12 +124,14 @@ public class IMUTeleOp extends MachHardware {
             //RUN CODE
             direction = determineDirection(0);
             telemetry.addData("Direction",direction);
-            move();
-            telemetry.addLine(doubleArrayToString(power));
-            intake();
-            telemetry.addLine("Spin: " + spin + ", Extend: " + extend + ", Vertical: " + vertical + ", Duck: " + dp);
 
-            if (gamepad1.y || gamepad2.y) {telemetry.update();} else {telemetry.clear();}
+            move();
+
+            telemetry.addLine(doubleArrayToString(power));
+
+            intake();
+
+            telemetry.addLine("Spin: " + spin + ", Extend: " + extend + ", Vertical: " + vertical + ", Duck: " + dp);
 
             motors[0].setPower(power[0]);
             motors[1].setPower(power[1]);
@@ -138,6 +141,8 @@ public class IMUTeleOp extends MachHardware {
             slide.setPower(extend);
             lift.setPower(vertical);
             duck.setPower(dp);
+
+            telemetry.update();
         }
     }
 
@@ -490,35 +495,59 @@ public class IMUTeleOp extends MachHardware {
 
         // At the beginning of each telemetry update, grab a bunch of data
         // from the IMU that we will then display in separate lines.
-        telemetry.addAction(() -> {
+        telemetry.addAction(new Runnable() { @Override public void run()
+        {
             // Acquiring the angles is relatively expensive; we don't want
             // to do that in each of the three items that need that info, as that's
             // three times the necessary expense.
             angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
             gravity  = imu.getGravity();
+        }
         });
 
         telemetry.addLine()
-                .addData("status", () -> imu.getSystemStatus().toShortString())
-                .addData("calib", () -> imu.getCalibrationStatus().toString());
+                .addData("status", new Func<String>() {
+                    @Override public String value() {
+                        return imu.getSystemStatus().toShortString();
+                    }
+                })
+                .addData("calib", new Func<String>() {
+                    @Override public String value() {
+                        return imu.getCalibrationStatus().toString();
+                    }
+                });
 
         telemetry.addLine()
-                .addData("heading", () -> formatAngle(angles.angleUnit, angles.firstAngle))
-                .addData("roll", () -> formatAngle(angles.angleUnit, angles.secondAngle))
-                .addData("pitch", () -> formatAngle(angles.angleUnit, angles.thirdAngle));
+                .addData("heading", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.firstAngle);
+                    }
+                })
+                .addData("roll", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.secondAngle);
+                    }
+                })
+                .addData("pitch", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.thirdAngle);
+                    }
+                });
 
         telemetry.addLine()
-                .addData("grvty", () -> gravity.toString())
-                .addData("mag", () -> String.format(Locale.getDefault(), "%.3f",
-                        Math.sqrt(gravity.xAccel*gravity.xAccel
-                                + gravity.yAccel*gravity.yAccel
-                                + gravity.zAccel*gravity.zAccel)));
-        telemetry.addLine()
-                .addData("grvty", () -> gravity.toString())
-                .addData("mag", () -> String.format(Locale.getDefault(), "%.3f",
-                        Math.sqrt(gravity.xAccel*gravity.xAccel
-                                + gravity.yAccel*gravity.yAccel
-                                + gravity.zAccel*gravity.zAccel)));
+                .addData("grvty", new Func<String>() {
+                    @Override public String value() {
+                        return gravity.toString();
+                    }
+                })
+                .addData("mag", new Func<String>() {
+                    @Override public String value() {
+                        return String.format(Locale.getDefault(), "%.3f",
+                                Math.sqrt(gravity.xAccel*gravity.xAccel
+                                        + gravity.yAccel*gravity.yAccel
+                                        + gravity.zAccel*gravity.zAccel));
+                    }
+                });
     }
 
     //----------------------------------------------------------------------------------------------
